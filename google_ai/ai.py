2841dc075d09bb9d2de9af8e553f1dab58937d1e
José <josegabrielpadua@gmail.com>
2025-05-15 16:12:30 -0300

    APP

--- google_ai/ai.py
+++ google_ai/ai.py
@@ -1,11 +1,26 @@
 from dotenv import load_dotenv
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import HumanMessage
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.text_splitter import CharacterTextSplitter
+
+loader = PyPDFLoader("documentos/documento_ai.pdf")
+documents = loader.load()
 
 load_dotenv()
 
 google_api_key = os.getenv('GOOGLE_API_KEY')
+
+text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+texts = text_splitter.split_documents(documents)
+
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+db = FAISS.from_documents(texts, embedding_model)
 
 
 class AIGoogle:
@@ -13,12 +28,13 @@ class AIGoogle:
         self.prompt_user = prompt_user
         self.response = response
         self.google_ai_llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+        self.context = "\n\n".join([d.page_content for d in texts])
 
 
     def interaction(self):
 
         messages = [
-            ("system", f"{self.prompt_user}"),
+            ("system", f"{self.prompt_user}\n\n{self.context}"),
             ("human", f"{self.response}"),
         ]
 
